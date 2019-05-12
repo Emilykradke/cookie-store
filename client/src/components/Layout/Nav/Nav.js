9dabfb11bd093887a2c57105223ebe44fd145811
Emilykradke <emilykradke@gmail.com>
2019-05-11 19:50:06 -0500

    nav and page routes

--- client/src/components/Layout/Nav/Nav.js
+++ client/src/components/Layout/Nav/Nav.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
+import "./nav.css"
 
 class Nav extends Component {
-  constructor(props) {
-    super(props);
-    
-  }
-  
-
   render() {
     return (
-      <div>
-        <h2>Nav</h2>
-      </div>
+      <nav>
+        <h2><a href="/">Cookie Store</a></h2>
+
+        <ul className="nav-list">
+          <li><a href="/">Home</a></li>
+          <li><a href="/Shop">Shop</a></li>
+          <li><a href="/AboutUs">About Us</a></li>
+          <li><a href="/ContactUs">Contact Us</a></li>
+          <li><a href="/SignIn">Sign In</a></li>
+          <li><a href="/Cart">Cart</a></li>
+        </ul>
+      </nav>
     );
   }
 }
