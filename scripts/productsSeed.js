ec6002be4e23ff741223c4d8c16fe4bcaf6a2d7d
Emilykradke <emilykradke@gmail.com>
2019-05-13 20:27:26 -0500

    seed

--- scripts/productsSeed.js
+++ scripts/productsSeed.js
@@ -112,9 +112,9 @@ db.Product
   .then(() => db.Product.collection.insertMany(productSeed))
   .then(data => {
     console.log(data.result.n + " records inserted!");
-    process.exit(0);
+    // process.exit(0);
   })
   .catch(err => {
     console.error(err);
-    process.exit(1);
+    // process.exit(1);
   });
