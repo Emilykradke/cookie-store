39381fb6c76aa57bd3283c42f3eb472627805a29
Emilykradke <emilykradke@gmail.com>
2019-05-18 16:00:56 -0500

    up to date

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,20 @@
 import React, { Component } from "react";
-import Container from "../components/Layout/Container/Container"
+import { Link } from "react-router-dom";
 
 class Home extends Component {
-render(){
-  return(
-    <Container>
-      <div className="hero" >
-        <h1 className="title">Cookie Store</h1>
+  render() {
+    return (
+      <div className="home-container">
+        <div className="hero">
+          <h1 className="hero-title">Cookie Store</h1>
+          <h2 className="hero-subtitle">COOOOOOOKIES</h2>
+          <Link className="btn-view-products" to="/Shop">
+            View all cookies
+          </Link>
+        </div>
       </div>
-    </Container>
-  ) 
-}
+    ) 
+  }
 };
 
 export default Home;
